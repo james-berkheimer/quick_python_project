1438da71c3aa1429f8ebdb58c513cd61081a4476
james-berkheimer <james.berkheimer@gmail.com>
2024-03-04 15:18:25 -0800

    Working code for directory and file generation

--- src/quick_project/main.py
+++ src/quick_project/main.py
@@ -1,6 +1,25 @@
 import os
 
+import click
+
+from quick_project import project_generation
+
+
+@click.command()
+@click.option("--name", prompt="Project name", help="The name of the project.")
+@click.option(
+    "--path",
+    prompt="Project path",
+    default=os.path.join(os.getenv("HOME"), "code"),
+    help="The path where the project will be created.",
+)
+def create_project(name, path):
+    """This is a simple CLI that accepts a project name and path as options."""
+    project_root_path = os.path.join(path, name)
+    project_generation.create_directories(name, project_root_path)
+    project_generation.generate_project_files(name, project_root_path)
+    click.echo(f"Created '{name}' project at '{project_root_path}'")
+
 
 def main():
-    print("Hello World!")
-    print("Current working directory: ", os.getcwd())
+    create_project()
---
+++ templates/README.md
@@ -0,0 +1,21 @@
+# Project: <PROJECT_NAME>
+
+## Description
+
+A brief description of your project goes here.
+
+## Installation
+
+Provide steps on how to install your project.
+
+## Usage
+
+Provide examples and explanations on how to use your project.
+
+## Contributing
+
+Provide instructions on how others can contribute to your project.
+
+## License
+
+Include information about the license here. Typically, this might be a simple statement like "This project is licensed under the MIT License - see the LICENSE file for details."
